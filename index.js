09cbfaf7cd3fc081634aaead08083af3d911bb95
Felix Kariuki <fkariuki1@live.co.uk>
2019-07-29 00:35:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var kittens = [ "Milo", "Otis", "Garfield" ]
 function destructivelyAppendKitten (Milo) {
   return kittens.push (Milo)
 }
+
+function destruvtivelyPrependKitten (Garfield) {
+  return kittens.unshift (Garfield)
+}
